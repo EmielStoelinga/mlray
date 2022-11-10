b6e958b3de4b6dc92bddea7793ebc02be56de067
Emiel Stoelinga <emielstoelinga@gmail.com>
2022-11-10 14:55:37 +0100

    tune lightgbm

--- src/tune_lightgbm.py
+++ src/tune_lightgbm.py
@@ -1,7 +1,11 @@
 import ray
+from ray import tune
 from ray.data.dataset import Dataset
 from ray.air.result import Result
+from ray.air import RunConfig, ScalingConfig
 from ray.train.lightgbm import LightGBMTrainer
+from ray.tune.tune_config import TuneConfig
+from ray.tune.tuner import Tuner
 from ray.air.config import ScalingConfig
 import tempfile
 import pandas as pd
@@ -31,7 +35,7 @@ def load_data():
     return train_dataset, test_dataset
 
 
-def train_lightgbm(target: str, train_dataset: Dataset, test_dataset: Dataset) -> Result:
+def tune_lightgbm(target: str, train_dataset: Dataset, test_dataset: Dataset) -> Result:
     trainer = LightGBMTrainer(
         scaling_config=ScalingConfig(
             # Whether to use GPU acceleration.
@@ -40,22 +44,34 @@ def train_lightgbm(target: str, train_dataset: Dataset, test_dataset: Dataset) -
         label_column=target,
         num_boost_round=20,
         params={
-            # LightGBM specific params
-            "objective": "regression",
-            "num_leaves": 31
+            "objective": "regression"
         },
         datasets={"train": train_dataset, "valid": test_dataset},
     )
-    result = trainer.fit()
-    return result
+
+    tuner = Tuner(
+        trainer,
+        run_config=RunConfig(verbose=1),
+        param_space={
+            "params": {
+                "num_leaves": tune.randint(25, 63), 
+            },
+        },
+        # max_concurrent_trials > 2 resulted in resource issues on local machine. This could be
+        # incremented when running on a cluster. 
+        tune_config=TuneConfig(num_samples=8, metric="valid-l2", mode="min", max_concurrent_trials=2),
+    )
+    result = tuner.fit()
+    best_result = result.get_best_result()
+    return best_result
 
 
 def main():
     target = 'WACHTTIJD'
     train_dataset, test_dataset = load_data()
-    result = train_lightgbm(target, train_dataset, test_dataset)
+    result = tune_lightgbm(target, train_dataset, test_dataset)
 
-    print(f"L2 validation score: {result.metrics['valid-l2']}")
+    print(f"L2 validation score best model = {result.metrics['valid-l2']} with num_leaves = {result.metrics['config']['params']['num_leaves']}.")
 
 
 if __name__ == '__main__':
